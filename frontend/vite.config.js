436175c423ab0ef69dd31459858a82327bcc7e48
Lunarawake <tashang6002@gmail.com>
2026-05-20 00:24:46 -0700

    fix vite build config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 3000,
     proxy: {
-      // 前端所有 /api/* 请求代理到后端
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
